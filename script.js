b69cd54b63eab86970f817f379d9ee7bd177db60
QianweiYu <130745435+QianweiYu@users.noreply.github.com>
2026-04-07 23:31:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,69 @@
-function showMessage() {
+function startSurprise(){
 
-    document.getElementById("secret").style.display = "block";
+document.getElementById("surprise").style.display="block";
 
-    document.getElementById("message").innerText =
-        "生日快乐 Happy Birthday❤️";
+confetti();
+
 }
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML="💖";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.animationDuration=(3+Math.random()*2)+"s";
+
+document.querySelector(".hearts").appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},5000);
+
+}
+
+setInterval(createHeart,300);
+
+
+function confetti(){
+
+for(let i=0;i<100;i++){
+
+let conf=document.createElement("div");
+
+conf.innerHTML="🎉";
+
+conf.style.position="fixed";
+
+conf.style.left=Math.random()*100+"vw";
+
+conf.style.top="-10px";
+
+conf.style.fontSize="25px";
+
+conf.style.animation="fall 3s linear";
+
+document.body.appendChild(conf);
+
+setTimeout(()=>conf.remove(),3000);
+
+}
+
+}
+
+const style=document.createElement("style");
+
+style.innerHTML=`
+
+@keyframes fall{
+0%{transform:translateY(-10px)}
+100%{transform:translateY(100vh)}
+}
+
+`;
+
+document.head.appendChild(style);
